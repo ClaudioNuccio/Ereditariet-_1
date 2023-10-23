1bf734d7c555b05596b1f6a40eb67b79510e73ab
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-23 10:50:18 +0200

    Added Animale and Gatto and declared on Main

---
+++ untitled/src/Animale.java
@@ -0,0 +1,6 @@
+public class Animale {
+    public void faiIlVerso() {
+        System.out.println("Sto facendo il verso!");
+
+    }
+}
---
+++ untitled/src/Gatto.java
@@ -0,0 +1,5 @@
+public class Gatto extends Animale{
+  @Override public void faiIlVerso() {
+        System.out.println("Sono un gatto e miagolo");
+    }
+}
