3bae453fdc5c45d31adce6a0afed72ea5297c486
Claudio Nuccio <cnuccio00@gmail.com>
2023-10-27 17:30:27 +0200

    added error message

--- String/src/Main.java
+++ String/src/Main.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static   void uniCode(String stringa,int posizione ){
-        int toUnicode = stringa.codePointAt(posizione);
 
-        StringBuilder risultato = new StringBuilder("Il carattere in unicode alla posizone scelta è ");
-        risultato.append(toUnicode);
 
-        System.out.println(risultato);
+        if (posizione < 0 || posizione>= stringa.length()){
+            System.out.println("Indice non valido");
+        }
+            else{
+            StringBuilder risultato = new StringBuilder("Il carattere in unicode alla posizone scelta è ");
+            int toUnicode = stringa.codePointAt(posizione);
+            risultato.append(toUnicode);
+            System.out.println(risultato);
+        }
     }
     public static void main (String []args){
-        uniCode("Virgulini" , 5);
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Inserisci la stringa da prendere in esame");
+        String test = scanner.nextLine();
+        System.out.println("Inserisci l'indice da esaminare");
+        int index  = scanner.nextInt();
+        uniCode(test,index);
     }
 }
